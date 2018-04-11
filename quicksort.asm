0f8223321b13882d078ef11de63742a747b3b81a
Pratik Gupta <32789120+pikabing@users.noreply.github.com>
2018-04-12 03:17:43 +0530

    Update quicksort.asm

--- quicksort.asm
+++ quicksort.asm
@@ -1,9 +1,7 @@
 #Pratik Gupta
 #Quicksort program in assembly
 # Email - pgcool33@gmail.com
-# For a refernce, I've used GeeksforGeeks code of bubblesort in C
-# You can take help from that, and I've changed the variables 'i' to 'c' and 'j' to 'd'.
-# Hope you'll be able to understand the concept and algorithm I've implemented. 
+
 
 .data 
 low: 	.word 0
